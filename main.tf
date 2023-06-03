1e8d3bb7602d408cb85ac2b72ebfaaa9a58740ed
Willian Paulino <willianpaulinox@gmail.com>
2023-06-03 19:38:58 -0300

    fetch files from course repo and add Financeiro and Salesforce folders and projects.

--- main.tf
+++ main.tf
@@ -1,13 +1,47 @@
 provider "googleworkspace" {
   credentials             = "${file("serviceaccount.yaml")}"
   customer_id             = "informaraqui"
-  impersonated_user_email = "emailaqui"
+  impersonated_user_email = "willian@willcloudskillboost.com.br"
   oauth_scopes = [
     "https://www.googleapis.com/auth/admin.directory.user",
     "https://www.googleapis.com/auth/admin.directory.userschema",
     "https://www.googleapis.com/auth/admin.directory.group",	
     # include scopes as needed
   ]
+}
+
+resource "google_folder" "Financeiro" {
+  display_name = "Financeiro"
+  parent = "organizations/812560220698"
+}
+
+resource "google_folder" "Salesforce" {
+  display_name = "Salesforce"
+  parent = google_folder.Financeiro.name
+}
+
+resource "google_folder" "Desenvolvimento" {
+  display_name = "Desenvolvimento"
+  parent = google_folder.Salesforce.name
+}
+
+resource "google_folder" "Producao" {
+  display_name = "Producao"
+  parent = google_folder.Salesforce.name
+}
+
+resource "google_project" "will-financeiro-salesforce-dev" {
+  name = "salesforce-dev"
+  project_id = "will-financeiro-salesforce-dev"
+  folder_id = google_folder.Desenvolvimento.name
+  auto_create_network=false
+}
+
+resource "google_project" "will-financeiro-salesforce-prd" {
+  name = "salesforce-prd"
+  project_id = "will-financeiro-salesforce-prd"
+  folder_id = google_folder.Producao.name
+  auto_create_network=false
 }
 
 resource "googleworkspace_group" "devops" {
